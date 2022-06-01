1d90bf7b02efdf36171713c76171020c151bae50
Animesh Samaiya <asamaiya00@gmail.com>
2022-06-01 23:41:50 +0530

    feat: add product to cart and manage inventory

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -2,18 +2,25 @@
   <div>
     <h1>Product list</h1>
     <ul>
-      <li v-for="product in products" :key="product.title">{{product.title}} - {{product.price}}</li>
+      <li v-for="product in products" :key="product.title">
+        {{ product.title }} - {{ product.price }}
+        <button @click="addProductToCart(product)">Add to cart</button>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'ProductList',
   computed: {
     products () {
       return this.$store.getters.availableProducts
+    }
+  },
+  methods: {
+    addProductToCart (product) {
+      this.$store.dispatch('addProductToCart', product)
     }
   },
   mounted () {
@@ -23,5 +30,4 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-</style>
+<style scoped></style>
--- src/store/index.js
+++ src/store/index.js
@@ -3,16 +3,26 @@ import shop from '@/api/shop'
 
 export default createStore({
   state: {
-    products: []
+    products: [],
+    cart: []
   },
   getters: {
     availableProducts (state) {
-      return state.products.filter(prod => prod.inventory > 0)
+      return state.products.filter((prod) => prod.inventory > 0)
     }
   },
   mutations: {
     setProducts (state, products) {
       state.products = products
+    },
+    pushProductToCart (state, productId) {
+      state.cart.push({ id: productId, quantity: 1 })
+    },
+    increaseItemQuantity (state, cartItem) {
+      cartItem.quantity++
+    },
+    decreaseProductInventory (state, product) {
+      product.inventory--
     }
   },
   actions: {
@@ -20,8 +30,18 @@ export default createStore({
       shop.getProducts((products) => {
         commit('setProducts', products)
       })
+    },
+    addProductToCart ({ state, commit }, product) {
+      if (product.inventory > 0) {
+        const cartItem = state.cart.find((item) => item.id === product.id)
+        if (!cartItem) {
+          commit('pushProductToCart', product.id)
+        } else {
+          commit('increaseItemQuantity', cartItem)
+        }
+        commit('decreaseProductInventory', product)
+      }
     }
   },
-  modules: {
-  }
+  modules: {}
 })
